007f8fd8e085c5ce41634828040e66a9303805be
Jamlit37 <james.v.little@hotmail.com>
2022-10-16 14:55:31 +1100

    bug fixes to get saved books to work

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,56 +1,55 @@
-
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
-import { useQuery, useMutation } from '@apollo/client';
-import { GET_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
-const SavedBooks = () => {
-  const { loading, data } = useQuery(GET_ME);
-  const [deleteBook] = useMutation(REMOVE_BOOK);
-  const userData = data?.me || {};
+// import useMutation and useQuery hook
+import { useMutation, useQuery } from '@apollo/client'
+// import REMOVE_BOOK mutation
+import { REMOVE_BOOK } from '../utils/mutations'
+// import GET_ME query
+import { GET_ME } from '../utils/queries'
 
-  if(!userData?.username) {
-    return (
-      <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
-      </h4>
-    );
+
+const SavedBooks = () => {
+  // destructure data and loading boolean from GET_ME graphql query
+  const { data, loading } = useQuery(GET_ME)
+
+  // define userData as a variable that can be changed
+  let userData
+
+  if(data) {
+    // if data exists, change userData value to data.me
+    userData = data.me
+  } else {
+    // if data does not exist, make userData an empty object
+    userData = {}
   }
+
+  // destructure removeBook function from REMOVE_BOOK graphql mutation
+  const [removeBook] = useMutation(REMOVE_BOOK)
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    if (!token) return false
 
     try {
-      await deleteBook({
-        variables: {bookId: bookId},
-        update: cache => {
-          const data = cache.readQuery({ query: GET_ME });
-          const userDataCache = data.me;
-          const savedBooksCache = userDataCache.savedBooks;
-          const updatedBookCache = savedBooksCache.filter((book) => book.bookId !== bookId);
-          data.me.savedBooks = updatedBookCache;
-          cache.writeQuery({ query: GET_ME , data: {data: {...data.me.savedBooks}}})
-        }
-      });
+      const { data } = await removeBook({ variables: { bookId } })
+
+      if (data == null) throw new Error('something went wrong!')
+
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
-  // if data isn't here yet, say so
-  if (loading) {
-    return <h2>LOADING...</h2>;
-  }
+
+  // this is the loading screen, when loading is true, this h2 element will be returned
+  if (loading) return <h2>LOADING...</h2>
 
   return (
     <>
@@ -73,10 +72,9 @@ const SavedBooks = () => {
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
-                  {book.link ? <Card.Text><a href={book.link} target="_blank" rel="noopener noreferrer">More Information on Google Books</a></Card.Text> : null}
                   <Card.Text>{book.description}</Card.Text>
                   <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
-                    Delete this Book
+                    Delete this Book!
                   </Button>
                 </Card.Body>
               </Card>
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -41,7 +41,6 @@ const resolvers = {
                 )
                 return updatedUser;
             }
-            throw new AuthenticationError('You need to be logged in!')
         },
         removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
